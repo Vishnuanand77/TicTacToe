390a781f28b482fe6f990b96f749176945075d04
Vishnu Anand <vishnuanand2000@gmail.com>
2020-10-21 10:59:30 +0530

    Designed basic app UI

--- app/src/main/java/com/vishnu/tictactoe/MainActivity.java
+++ app/src/main/java/com/vishnu/tictactoe/MainActivity.java
@@ -3,8 +3,20 @@ package com.vishnu.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView playerOneScore, playerTwoScore, playerStatus;
+    private Button [] buttons = new Button[9];
+    private Button resetGame;
+
+    private int playerOneScoreCount, playerTwoScoreCount, roundCount;
+    boolean activePlayer;
+
+    //States:
+    //p1 -> 0, p2 ->1, empty -> 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
